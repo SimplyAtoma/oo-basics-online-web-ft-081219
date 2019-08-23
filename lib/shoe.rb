1731f5c8d989b3490921f848c0b0b9eb1f3e35b0
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-23 18:08:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
 # Make your shoe class here!
+class Shoe 
+  def initialize(new_brand)
+    @brand = new_brand
+  end 
+  
+  attr_writer :brand 
+  attr_reader :brand
+  attr_accessible :brand
+end
